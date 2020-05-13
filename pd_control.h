f53ed0ba52c358f4320dd483d16e03249510df3b
solotims <47323823+solotims@users.noreply.github.com>
2020-05-13 05:47:43 +0300

    Update pd_control.h

--- pd_control.h
+++ pd_control.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+//ПД-регулятор, нога стремиться принять нужную позицию наикратчайшим путем по кругу
+
 float pd_controller(float theta_act, float theta_des, float v_act, float v_des, float kp, float kd){
  	float diff = fmod(theta_des - theta_act, 360);
  	float shortest_distance = 180 - fabs(fabs(diff) - 180);
