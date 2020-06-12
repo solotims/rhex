427c18f125ea486fa1338ecfd78f9e89bca50036
solotims <47323823+solotims@users.noreply.github.com>
2020-06-12 07:57:18 +0300

    Add files via upload

--- remaster/ardu.ino
+++ remaster/ardu.ino
@@ -27,18 +27,12 @@ void loop(){
   
     actual_theta = //Текущий угол двигателя в градусах, лежащий в пределах от 0 до 360;
 	
-	//Так как двигатели одинаковые, то противоположные двигатели движутся в разном направлении
-    if (legs[i].right){ 
-        desired_theta = get_angle(millis(), legs[i]); 
-    }
-    else{ 
-        desired_theta = -get_angle(millis(), legs[i]);
-    }
+	desired_theta = get_angle(millis(), legs[i]); //желаемый в данный момент угол
 	
     control_signal = p_controller(actual_theta, desired_theta, kp);  //П-регулятор
 	
     finput = legs[i].b_v + control_signal;   //Подаваемый сигнал - сумма постоянной составляющей и П-регулятора
 	
-    //Подача сигнала finput на двигатель 
+    //Подача сигнала finput на двигатель, нужно учесть, что у противоположных двигателей напряжение должно быть разных знаков 
 	
 }
