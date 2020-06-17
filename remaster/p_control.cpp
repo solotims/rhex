65718c1382a559f11a02b615985047524ca092fe
solotims <47323823+solotims@users.noreply.github.com>
2020-06-18 02:01:32 +0300

    Add files via upload

--- remaster/p_control.cpp
+++ remaster/p_control.cpp
@@ -3,8 +3,8 @@
 #include <math.h>
 
 float p_controller(float theta_act, float theta_des, float k){
- 	float diff = theta_des - theta_act;   //Разность между желаемым и текущим углом поворота 
+ 	float diff = fmod(theta_des - theta_act, 360);   //Разность между желаемым и текущим углом поворота 
  	float shortest_distance = 180 - fabs(fabs(diff) - 180);   //Кратчаешее расстояние на окружности между углами
- 	float theta_dist = diff < 180 ? shortest_distance : -shortest_distance;  //В зависимости от того, спешит ли или опаздыет ли двигатель, выбирается знак
- 	return k * theta_dist;  
+ 	float theta_dis = fmodf(diff + 360, 360) < 180 ? shortest_distance : -shortest_distance;  //В зависимости от того, спешит ли или опаздыет ли двигатель, выбирается знак
+ 	return k * theta_dis;  
 }
